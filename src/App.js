0bf38867c7274b2349ccb5daea87f74fd2b425e0
vanbumi <jsp.dio@gmail.com>
2020-05-07 13:41:27 +0700

    bab2 done, memisahkan homepage kedalam masing2 component kecil, selesai styling pada menu item

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import HomePage from './HomepageComponent';
+import HomePage from './pages/homepage/HomepageComponent';
 
 function App() {
   return (
---
+++ src/pages/homepage/HomepageComponent.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './homepageStyles.scss';
+import Directory from '../../components/directory/DirectoryComponent';
+
+
+const HomepageComponent = () =>(
+    <div className='homepage'>
+        <Directory/>
+    </div>
+)
+
+export default HomepageComponent
