9aac7c83843eb5ce6a6a3b8de6266e2c7828706d
vanbumi <jsp.dio@gmail.com>
2020-05-08 14:29:16 +0700

    bab7 done, membuat form sign in

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import { Switch, Route } from 'react-router-dom';
 import HomePage from './pages/homepage/HomepageComponent';
 import ShopPage from './pages/shop/ShopComponent';
+import SignInAndSignUpPage from './pages/signIn-signUp/SignIn-SignUpComponent';
 import Header from './components/header/HeaderComponent';
 
 function App() {
@@ -12,6 +13,7 @@ function App() {
       <Switch>
         <Route exact path='/' component={HomePage} />
         <Route path='/shop' component={ShopPage} />
+        <Route path='/signin' component={SignInAndSignUpPage} />
       </Switch>
     </div>
   );
---
+++ src/pages/signIn-signUp/SignIn-SignUpComponent.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import SignIn from '../../components/sign-in/SignInComponent';
+
+import './signIn-signUpStyle.scss';
+
+const SignInAndSignUpPage = () => (
+
+    <div className='sign-in-and-sign-up'>
+        <SignIn/>
+    </div>
+);
+
+export default SignInAndSignUpPage;
