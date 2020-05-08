3d4f94fed48f7fb2bc50b56df187b352d52f61d5
vanbumi <jsp.dio@gmail.com>
2020-05-08 12:06:50 +0700

    bab 5 done, membuat collection item dan update collectin preview component

--- src/components/collection-preview/CollectionPreview.js
+++ src/components/collection-preview/CollectionPreview.js
@@ -1,20 +1,18 @@
 import React from 'react';
 
-//import CollectionItem from '../collection-item/collection-item.component';
+import CollectionItem from '../collection-item/CollectionItemComponent';
 
-//import './collection-preview.styles.scss';
+import './collectionPreviewStyle.scss';
 
 const CollectionPreview = ({ title, items }) => (
   <div className='collection-preview'>
     <h1 className='title'>{title.toUpperCase()}</h1>
     <div className='preview'>
-          {items
-            .filter((item, idx) => idx < 4)
-            .map(item => (
-                <div key={item.id}>
-                    {item.name}
-                </div>
-            ))}
+    {items
+        .filter((item, idx) => idx < 4)
+        .map(({ id, ...otherItemProps }) => (
+          <CollectionItem key={id} {...otherItemProps} />
+        ))}
     </div>
   </div>
 );
