c85487c9aa385371e3c4e3c2e6b899ac54187e2e
vanbumi <jsp.dio@gmail.com>
2020-05-12 10:08:11 +0700

    Materi bab 12 done, handle auth change on firebase

--- src/App.js
+++ src/App.js
@@ -5,18 +5,43 @@ import HomePage from './pages/homepage/HomepageComponent';
 import ShopPage from './pages/shop/ShopComponent';
 import SignInAndSignUpPage from './pages/signIn-signUp/SignIn-SignUpComponent';
 import Header from './components/header/HeaderComponent';
+import { auth } from './firebase/firebase.utils';
 
-function App() {
-  return (
-    <div>
-      <Header/>
-      <Switch>
-        <Route exact path='/' component={HomePage} />
-        <Route path='/shop' component={ShopPage} />
-        <Route path='/signin' component={SignInAndSignUpPage} />
-      </Switch>
-    </div>
-  );
+class App extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      currentUser: null 
+    }
+  }
+
+  unsubscribeFromAuth = null;
+  
+  componentDidMount() {
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(user => {
+      this.setState({ currentUser: user });
+
+      console.log(user);
+    });
+  }
+
+  componentWillUnmount() {
+    this.unsubscribeFromAuth();
+  }
+
+  render() {
+    return (
+      <div>
+        <Header/>
+        <Switch>
+          <Route exact path='/' component={HomePage} />
+          <Route path='/shop' component={ShopPage} />
+          <Route path='/signin' component={SignInAndSignUpPage} />
+        </Switch>
+      </div>
+    );
+  } 
 }
 
 export default App;
