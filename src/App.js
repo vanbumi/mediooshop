e5db217e1720c2724cfffc9d4904195554587a1a
vanbumi <jsp.dio@gmail.com>
2020-05-15 10:32:11 +0700

    Materi bab 24 done, User Redirect dan User Action Type

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import { Switch, Route } from 'react-router-dom';
+import { Switch, Route, Redirect } from 'react-router-dom';
 import HomePage from './pages/homepage/HomepageComponent';
 import ShopPage from './pages/shop/ShopComponent';
 import SignInAndSignUpPage from './pages/signIn-signUp/SignIn-SignUpComponent';
@@ -42,18 +42,32 @@ class App extends React.Component {
         <Switch>
           <Route exact path='/' component={HomePage} />
           <Route path='/shop' component={ShopPage} />
-          <Route path='/signin' component={SignInAndSignUpPage} />
+          <Route
+            exact 
+            path='/signin'
+            render={() => 
+              this.props.currentUser ? (
+                <Redirect to='/' />
+              ) : (
+                <SignInAndSignUpPage/>
+              )
+            }
+          />   
         </Switch>
       </div>
     );
   } 
 }
 
+const mapStateToProps = ({ user }) => ({
+  currentUser: user.currentUser
+});
+
 const mapDispatchToProps = dispatch => ({
   setCurrentUser: user => dispatch(setCurrentUser(user))
 });
 
 export default connect(
-  null,
+  mapStateToProps,
   mapDispatchToProps
 )(App);
