b1b34c45160c0eb4a8cd9a1f910b34a18cfd571f
vanbumi <jsp.dio@gmail.com>
2020-05-19 13:23:28 +0700

    Materi bab 31 done, art Item Component

--- src/components/cart-dropdown/CartDropDownComponent.js
+++ src/components/cart-dropdown/CartDropDownComponent.js
@@ -1,14 +1,24 @@
 import React from 'react';
+import { connect } from 'react-redux';
 
 import CustomButton from '../custom-button/CustomButtonComponent';
+import CartItem from '../cart-item/CartItemComponent';
 
 import './cartDropdownStyle.scss';
 
-const CartDropdown = () => (
+const CartDropdown = ({ cartItems }) => (
   <div className='cart-dropdown'>
-    <div className='cart-items' />
+    <div className='cart-items'>
+      {cartItems.map(cartItem => (
+        <CartItem key={cartItem.id} item={cartItem} />
+      ))}
+    </div>
     <CustomButton>GO TO CHECKOUT</CustomButton>
   </div>
 );
 
-export default CartDropdown;
+const mapStateToProps = ({ cart: { cartItems } }) => ({
+  cartItems
+});
+
+export default connect(mapStateToProps)(CartDropdown);
