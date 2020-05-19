2672ad19ee690ac2eaef909e5baeeae964c63e42
vanbumi <jsp.dio@gmail.com>
2020-05-19 14:22:40 +0700

    Materi bab 32 done, Selector Redux dan Reselect Library

--- src/components/cart-icon/CartIconComponent.js
+++ src/components/cart-icon/CartIconComponent.js
@@ -1,14 +1,17 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import { ReactComponent as ShoppingIcon } from '../../assets/shopping-bag.svg';
+
 import { toggleCartHidden } from '../../redux/cart/cartActions';
+import { selectCartItemsCount } from '../../redux/cart/cartSelectors';
+
+import { ReactComponent as ShoppingIcon } from '../../assets/shopping-bag.svg';
 
 import './cartIconStyle.scss';
 
-const CartIcon = ({ toggleCartHidden }) => (
+const CartIcon = ({ toggleCartHidden, itemCount }) => (
   <div className='cart-icon' onClick={toggleCartHidden}>
     <ShoppingIcon className='shopping-icon' />
-    <span className='item-count'>0</span>
+    <span className='item-count'>{itemCount}</span>
   </div>
 );
 
@@ -16,8 +19,11 @@ const mapDispatchToProps = dispatch => ({
   toggleCartHidden: () => dispatch(toggleCartHidden())
 });
 
+const mapStateToProps = state => ({
+  itemCount: selectCartItemsCount(state)
+});
+
 export default connect(
-  null,
+  mapStateToProps,
   mapDispatchToProps
 )(CartIcon);
-
