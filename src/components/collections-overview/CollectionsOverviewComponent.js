d23da5fbd32252dc56d26ce69a1a83976503fe91
vanbumi <jsp.dio@gmail.com>
2020-06-04 12:07:43 +0700

    bab 43 done, Collection Overview

---
+++ src/components/collections-overview/CollectionsOverviewComponent.js
@@ -0,0 +1,25 @@
+import React from "react";
+import { connect } from "react-redux";
+import { createStructuredSelector } from "reselect";
+
+import CollectionPreview from "../collection-preview/CollectionPreview";
+
+import { selectCollections } from "../../redux/shop/shopSelectors";
+
+import "./collectionsOverviewStyle.scss";
+
+const CollectionsOverview = ({ collections }) => {
+  return (
+    <div className="collections-overview">
+      {collections.map(({ id, ...otherCollectionProps }) => (
+        <CollectionPreview key={id} {...otherCollectionProps} />
+      ))}
+    </div>
+  );
+};
+
+const mapStateToProps = createStructuredSelector({
+  collections: selectCollections,
+});
+
+export default connect(mapStateToProps)(CollectionsOverview);
