a0f154a10ed7ee9dfe4aa408930a9d8b1a867712
vanbumi <jsp.dio@gmail.com>
2020-06-06 20:15:32 +0700

    bab 45 done, Improve nama component

---
+++ src/pages/collection/CollectionComponent.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import CollectionItem from '../../components/collection-item/CollectionItemComponent'; 
+
+import './collectionStyle.scss';
+
+const CollectionPage = ({ match }) => (
+    <div className='collection-page'>
+        <h2>Collection Page</h2>
+    </div>
+)
+
+export default CollectionPage;
--- src/pages/shop/ShopComponent.js
+++ src/pages/shop/ShopComponent.js
@@ -2,13 +2,13 @@ import React from 'react';
 import {Route} from 'react-router-dom';
 
 import CollectionsOverview from "./../../components/collections-overview/CollectionsOverviewComponent";
-import CategoryPage from '../category/CategoryComponent';
+import CollectionPage from '../collection/CollectionComponent';
 
 const ShopPage = ({ match }) => {
     return (
         <div className='shop-page'>
             <Route exact path={`${match.path}`} component={CollectionsOverview} />
-            <Route exact path={`${match.path}/:categoryId`} component={CategoryPage} />
+            <Route exact path={`${match.path}/:collectionId`} component={CollectionPage} />
         </div>
     )
 }
